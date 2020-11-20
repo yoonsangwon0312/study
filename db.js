0801192473de1c4e066267e222088046f0b0d327
yoonsangwon0312 <69022143+yoonsangwon0312@users.noreply.github.com>
2020-11-20 14:48:43 +0900

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-var dbInfo = {
-
-    host: 'ls-712a3de0f216372c332622b5ed5c6f22fe2f67bd.cu0xyssgzj43.ap-northeast-2.rds.amazonaws.com',
-    port: '3306',
-    user: 'dbmasteruser',
-    password:'buackr!!##',
-    database: 'BU',
-    multipleStatements: true
-}
